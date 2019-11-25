d0f062f05f72eec4478b2b0bd2309a0a5c73c4f8
Samuel Kerosi <samuelbosire60@gmail.com>
2019-11-25 17:05:16 -0600

    all done
    Shows
    - accuracy
    - predictions
    - confusion matrix
    -recall

--- knn.py
+++ knn.py
@@ -1,61 +1,58 @@
 import numpy as np
-from collections import Counter
-import pandas as pd
+from sklearn import datasets, metrics
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, precision_score, recall_score
+
+#  testting and training files
+training_file = np.genfromtxt("buyTraining.txt")
+testing_file = np.genfromtxt("buyTesting.txt")
+
+#  train stuff
+X_train = training_file[:, :-1]
+y_train = training_file[:, -1]
+# test stuff
+X_test = testing_file[:, :-1]
+y_test = testing_file[:, -1]
 
 
-def dummy():
-    print("hellooo world!")
+# create classifier and train usng the training sets
+knn_classifier = KNeighborsClassifier(n_neighbors=3, metric='euclidean')
+knn_classifier.fit(X_train, y_train)
 
-# calculate the euclidean distance
+#  prediction using test data
+predictiton = knn_classifier.predict(X_test)
+print(predictiton)
 
+# confuion matrices
+cm = confusion_matrix(y_test, predictiton)
+print("Confusion Matrix \n", cm)
 
-def euclidean_distance(x1, x2):
-    np.sqrt(np.sum((x1-x2)**2))
+# precision
+precision = precision_score(y_test, predictiton)
+print("precision: \n", precision)
 
-# to read training and testing datasets
+# recall
+recall = recall_score(y_test, predictiton,  average=None)
+print("Reecall: \n", recall)
 
+# accuracy
+accuracy = metrics.accuracy_score(y_test, predictiton)
+print("accuracy: \n", accuracy)
 
-def read_file(file):
-    df = pd.read_csv(file)
-    return df
-
-# to write the output to a file
-
-
-def output_file(file):
-    file = open(file, 'w')
-    # to be finished later
-
-
-def accuracy(y_true, y_pred):
-    accuracy = np.sum(y_true == y_pred) / len(y_true)
-    return accuracy
-
-
-def fit(self, X, y):
-    self.X_train = X
-    self.y_train = y
-
-
-def predict(self, X):
-    predicted_labels = [self._predcit(x) for x in X]
-    return np.array(predicted_labels)
-
-
-def _predcit(self, x):
-        # distances
-    distance = [euclidean_distance(x, x_train) for x_train in self.X_train]
-    # get the nearest neighbor
-    k_index = np.argsort(distance)[:self.k]
-    k_nearest_label = [self.y_train[i] for i in k_index]
-
-    # get the most common class label
-    most_common = Counter(k_nearest_label).most_common(1)
-    return most_common[0][0]
-
-
-if __name__ == "__main__":
-    training_data = read_file("buyTraining.txt")
-    testing_data = read_file("buytesting.txt")
-    print(training_data, testing_data)
+# output file
+outfile = open("knnmodels_output.txt", "w")
+outfile.write("Knn Model Output")
+outfile.write("\n")
+outfile.write("Precision: \n")
+outfile.write(str(precision))
+outfile.write("\n")
+outfile.write("Recall: \n")
+outfile.write(str(recall))
+outfile.write("\n")
+outfile.write("Confusion matrix: \n")
+outfile.write(str(cm))
+outfile.write("\n")
+outfile.write("Accuracy: \n")
+outfile.write(str(accuracy))
+outfile.close()
